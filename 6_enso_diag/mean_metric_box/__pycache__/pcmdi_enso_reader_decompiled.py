0c6e6c49e46e53ba574ef86353d0fcafb3c92d59
ShixuanZhang <shixuan.zhang@pnnl.gov>
2026-06-05 00:42:36 -0700

    remove unused file

--- 6_enso_diag/mean_metric_box/__pycache__/pcmdi_enso_reader_decompiled.py
+++
@@ -1,9 +0,0 @@
-# decompyle3 version 3.9.3
-# Python bytecode version base 3.13.0 (3571)
-# Decompiled from: Python 3.10.10 | packaged by conda-forge | (main, Mar 24 2023, 20:08:06) [GCC 11.3.0]
-# Embedded file name: /lcrc/group/e3sm/ac.szhang/acme_scratch/e3sm_project/v3_le_paper/6_enso_diag/pcmdi_enso_reader.py
-# Compiled at: 2025-12-05 03:07:50
-# Size of source mod 2**32: 22762 bytes
-
-Unsupported Python version, 3.13.0, for decompilation
-
